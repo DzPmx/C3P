022660cc5fc27ae973a2cdf6ea51da647a486fef
DzPmx <2431436884@qq.com>
2024-06-01 16:39:25 +0800

    Inital

---
+++ Code/02.01.03.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Dz on 24-6-1.
+//
+
+#include "iostream"
+int main() {
+    //2.5
+    //a //char// wchar_t// char[2]// wchar_t[2]
+    //b //int // unsigned int// long //unsigned long //8 int //16 int
+    //c //double//float//long double
+    //d //int //unsigned int//double//double
+
+    //2.6
+    //第一组是十进制
+    //第二组第一个无效，第二个是八进制
+    int month=07;
+
+    //2.7
+    //char //long double//float //long double
+
+    std::cout<<"2\115"<<std::endl;
+    std::cout<<2<<"\t"<<"\115"<<std::endl;
+
+}
