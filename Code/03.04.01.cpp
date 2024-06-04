d76a0dada7c64091da919a26d1a54a0f219e428b
DzPmx <2431436884@qq.com>
2024-06-05 02:14:12 +0800

    Up

---
+++ Code/03.04.01.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Dz on 24-6-4.
+//
+#include "cctype"
+#include "iostream"
+#include "string"
+#include "vector"
+
+
+int main() {
+
+    // std::vector<std::string> vstring;
+    // std::string input;
+    // for (int i = 0; i < 3; ++i) {
+    //     std::cin >> input;
+    //     vstring.push_back(input);
+    // }
+    // for (auto j=vstring.begin(); j!=vstring.end(); ++j) {
+    //     for(auto s=j->begin();s!=j->end();++s) {
+    //         *s=toupper(*s);
+    //     }
+    // }
+    //
+    // for (const auto& basic_string: vstring) {
+    //     std::cout << basic_string<<std::endl;
+    // }
+
+
+    // std::vector<std::string> text{"hello","","world"};
+    //
+    // for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) {
+    //     for (auto &ch: *it) {
+    //         ch = toupper(ch);
+    //     }
+    // }
+    //
+    // for (const auto &basic_string: text) {
+    //     std::cout << basic_string << std::endl;
+    // }
+
+    // std::vector<int> vec(10,50);
+    // for(auto value=vec.begin();value!=vec.end();++value) {
+    //     *value*=2;
+    // }
+    //
+    // for (const auto value: vec) {
+    //     std::cout << value<<std::endl;
+    //
+    // }
+}
---
+++ Code/03.04.02.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Dz on 24-6-5.
+//
+
+#include "cctype"
+#include "iostream"
+#include "string"
+#include "vector"
+
+int main() {
+
+}
